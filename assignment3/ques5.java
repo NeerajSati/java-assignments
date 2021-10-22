0da2a4f6d6514c4bb3997f5cbe7f5585e95a3a20
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-10-22 21:07:30 +0530

    question 5 completed

--- assignment3/ques5.java
+++ assignment3/ques5.java
@@ -10,6 +10,51 @@
 
 package assignment3;
 
+abstract class Employee {
+    String name;
+    int salary, grossSalary;
+
+    public abstract int calculategrosssalary();
+
+    public abstract int annualIncrement();
+
+}
+
+class Manager extends Employee {
+    Manager() {
+        super();
+    }
+
+    Manager(String name, int salary, int grossSalary) {
+        this.name = name;
+        this.salary = salary;
+        this.grossSalary = grossSalary;
+    }
+
+    public int calculategrosssalary() {
+        return grossSalary;
+    }
+
+    public int annualIncrement() {
+        return 15;
+    }
+}
+
+class SubManager extends Manager {
+    SubManager(String name, int salary, int grossSalary) {
+        super(name, salary, grossSalary);
+    }
+
+    public int calculategrosssalary() {
+        return grossSalary + 2000;
+    }
+}
+
 public class ques5 {
-    
+    public static void main(String[] args) {
+        Manager har = new Manager("Kailash", 10000, 11000);
+        System.out.println(har.calculategrosssalary());
+        SubManager sub = new SubManager("Kailash", 10000, 11000);
+        System.out.println(sub.calculategrosssalary());
+    }
 }
